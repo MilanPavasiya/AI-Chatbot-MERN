7b7765ce776898185550a7b6d5381dfb4137868f
MILAN PAVASIYA <MILPAVASIYA@GMAIL.COM>
2024-08-14 18:58:01 -0400

    Delete backend/dist directory

--- backend/dist/app.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { config } from 'dotenv';
-config();
-const app = express();
-// Middlewares
-app.use(express.json());
-export default app;
-//# sourceMappingURL=app.js.map
