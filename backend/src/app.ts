f77d15691a79533ced3d5880e09a596c5a9cfdce
MilanPavasiya <milpavasiya@gmail.com>
2024-08-16 15:35:11 -0400

    commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { config } from 'dotenv';
 import morgan from 'morgan';
+import appRouter from './routes/index.js';
 
 config();
 const app = express();
@@ -10,6 +11,6 @@ app.use(express.json());
 
 app.use(morgan('dev'));
 
-app.use('/api/v1');
+app.use('/api/v1', appRouter);
 
 export default app;
