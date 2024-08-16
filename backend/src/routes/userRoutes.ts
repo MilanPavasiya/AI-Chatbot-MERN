fbe9b2d6be9fa132da4c55267ef3977160a84341
MilanPavasiya <milpavasiya@gmail.com>
2024-08-16 15:32:30 -0400

    Validation for login, signup services

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,19 @@
 import { Router } from 'express';
-import { getAllUsers } from '../controllers/userControllers.js';
+import {
+	getAllUsers,
+	userLogin,
+	userSignup,
+} from '../controllers/userControllers.js';
+import {
+	loginValidator,
+	signupValidator,
+	validate,
+} from '../utils/validators.js';
 
 const userRoutes = Router();
 
 userRoutes.get('/', getAllUsers);
+userRoutes.post('/signup', validate(signupValidator), userSignup);
+userRoutes.post('/login', validate(loginValidator), userLogin);
 
 export default userRoutes;
