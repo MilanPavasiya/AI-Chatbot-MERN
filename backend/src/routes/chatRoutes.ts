2d09727607ff03bb4c63c71e62ed52720884067e
MilanPavasiya <milpavasiya@gmail.com>
2024-08-16 17:23:08 -0400

    routes updated

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,6 +1,21 @@
 import { Router } from 'express';
+import { verifyToken } from '../utils/tokenManager.js';
+import { chatCompletionValidator, validate } from '../utils/validators.js';
+import {
+	deleteChats,
+	generateChatCompletion,
+	sendChatsToUser,
+} from '../controllers/chatControllers.js';
 
 //Protected API
 const chatRoutes = Router();
+chatRoutes.post(
+	'/new',
+	validate(chatCompletionValidator),
+	verifyToken,
+	generateChatCompletion
+);
+chatRoutes.get('/all-chats', verifyToken, sendChatsToUser);
+chatRoutes.delete('/delete', verifyToken, deleteChats);
 
 export default chatRoutes;
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -2,18 +2,23 @@ import { Router } from 'express';
 import {
 	getAllUsers,
 	userLogin,
+	userLogout,
 	userSignup,
+	verifyUser,
 } from '../controllers/userControllers.js';
 import {
 	loginValidator,
 	signupValidator,
 	validate,
 } from '../utils/validators.js';
+import { verifyToken } from '../utils/tokenManager.js';
 
 const userRoutes = Router();
 
 userRoutes.get('/', getAllUsers);
 userRoutes.post('/signup', validate(signupValidator), userSignup);
 userRoutes.post('/login', validate(loginValidator), userLogin);
+userRoutes.get('/auth-status', verifyToken, verifyUser);
+userRoutes.get('/logout', verifyToken, userLogout);
 
 export default userRoutes;
