3ea8308fed7154c3b97e18933705f7384d956c35
MilanPavasiya <milpavasiya@gmail.com>
2024-08-16 15:33:55 -0400

    created a controller with a few services such as login, signup and so on

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -1,3 +1,66 @@
-export const getAllUsers = () => {
-	//get all users
+import { Request, Response, NextFunction } from 'express';
+import User from '../models/User.js';
+import { hash, compare } from 'bcrypt';
+
+export const getAllUsers = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		//get all users
+		const users = await User.find();
+		return res.status(200).json({ message: 'OK', users });
+	} catch (error) {
+		console.log(error);
+		return res.status(200).json({ message: 'ERROR', cause: error.message });
+	}
+};
+
+export const userSignup = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		//user signup
+		const { name, email, password } = req.body;
+		const existingUser = await User.findOne({ email });
+		if (existingUser) return res.status(401).send('User already registered');
+		const hashedPassword = await hash(password, 10);
+		const user = new User({ name, email, password: hashedPassword });
+		await user.save();
+
+		return res.status(200).json({ message: 'OK', id: user._id.toString() });
+	} catch (error) {
+		console.log(error);
+		return res.status(200).json({ message: 'ERROR', cause: error.message });
+	}
+};
+
+export const userLogin = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		//user login
+		const { email, password } = req.body;
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(401).send('User not registered');
+		}
+
+		const isPasswordCorrect = await compare(password, user.password);
+		if (!isPasswordCorrect) {
+			return res.status(403).send('Incorrect Password');
+		}
+
+		return res
+			.status(200)
+			.json({ message: 'OK', name: user.name, email: user.email });
+	} catch (error) {
+		console.log(error);
+		return res.status(200).json({ message: 'ERROR', cause: error.message });
+	}
 };
