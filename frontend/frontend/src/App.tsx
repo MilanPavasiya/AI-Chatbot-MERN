e3ec1144d78c10a3813afe8f913313fc6d5df5bb
MilanPavasiya <milpavasiya@gmail.com>
2024-08-20 22:28:57 -0400

    Routing configured and Styles implemented root components

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Chat from './pages/Chat';
+import NotFound from './pages/NotFound';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const auth = useAuth();
 
 	return (
-		<>
-			<h1>Hello</h1>
-		</>
+		<main>
+			<Header />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/signup' element={<Signup />} />
+				<Route path='/chat' element={<Chat />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+		</main>
 	);
 }
 
--- frontend/frontend/src/main.tsx
+++ frontend/frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
+const theme = createTheme({
+	typography: {
+		fontFamily: 'Roboto Slab,serif',
+		allVariants: { color: 'white' },
+	},
+});
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<Toaster position='top-right' />
+				<App />
+			</ThemeProvider>
+		</BrowserRouter>
+	</StrictMode>
+);
