82c895a8c44660061a8d806c2f6301b6e03ea087
MilanPavasiya <milpavasiya@gmail.com>
2024-08-21 21:42:10 -0400

    refactored the header component

--- frontend/frontend/src/components/Header.tsx
+++ frontend/frontend/src/components/Header.tsx
@@ -1,59 +1,55 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Logo from './shared/Logo';
-import { useAuth } from '../context/AuthContext';
-import NavigationLink from './shared/NavigationLink';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
 
 const Header = () => {
-	const auth = useAuth();
-
-	if (!auth) {
-		// Handle case where auth context is not available (null)
-		return null;
-	}
-
-	const { isLoggedIn, logout } = auth;
-
-	const renderAuthLinks = () => (
-		<>
-			<NavigationLink
-				bg='#00fffc'
-				to='/chat'
-				text='Go To Chat'
-				textColor='black'
-			/>
-			<NavigationLink
-				bg='#51538f'
-				textColor='white'
-				to='/'
-				text='Logout'
-				onClick={logout}
-			/>
-		</>
-	);
-
-	const renderGuestLinks = () => (
-		<>
-			<NavigationLink bg='#00fffc' to='/login' text='Login' textColor='black' />
-			<NavigationLink
-				bg='#51538f'
-				textColor='white'
-				to='/signup'
-				text='Signup'
-			/>
-		</>
-	);
-
-	return (
-		<AppBar
-			sx={{ bgcolor: 'transparent', position: 'static', boxShadow: 'none' }}>
-			<Toolbar sx={{ display: 'flex' }}>
-				<Logo />
-				<div>{isLoggedIn ? renderAuthLinks() : renderGuestLinks()}</div>
-			</Toolbar>
-		</AppBar>
-	);
+  const auth = useAuth();
+  return (
+    <AppBar
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
+    >
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/chat"
+                text="Go To Chat"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/"
+                text="logout"
+                onClick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/login"
+                text="Login"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/signup"
+                text="Signup"
+              />
+            </>
+          )}
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
-export default React.memo(Header);
+export default Header;
